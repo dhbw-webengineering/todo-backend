a019bcf5f155bcce35e5b57b50021fd594b203d2
knoepsim <simon.info.jc@gmail.com>
2025-06-24 22:22:56 +0200

    added cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors";
 import { authRoutes } from "./routes/authRoutes";
 import { passwordResetRoutes } from "./routes/passwordResetRoutes";
 import { todoRoutes } from "./routes/todosRoutes";
@@ -8,6 +9,11 @@ import userRoutes from "./routes/userRoutes";
 
 const app = Fastify();
 
+app.register(cors, {
+  origin: true, // <-- erlaubt alle Origins
+  credentials: true, // falls du Cookies/Credentials brauchst
+});
+
 app.register(rateLimit);
 app.register(jwtPlugin);
 app.register(authRoutes);
@@ -15,7 +21,7 @@ app.register(passwordResetRoutes);
 app.register(todoRoutes);
 app.register(userRoutes)
 
-app.listen({ port: 3000 }, (err, address) => {
+app.listen({ port: 3001 }, (err, address) => {
   if (err) throw err;
   console.log(`Server listening at ${address}`);
 });
